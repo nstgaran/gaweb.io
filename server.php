05a55fc8c2c91d0230eb03649f2b8c38df6d8ba3
nstgaran <danhtuongh@gmail.com>
2024-05-17 18:36:12 +0700

    Add files via upload

---
+++ server.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+$uri2 = $_SERVER['REQUEST_URI'] ;
+
+$uri = urldecode(
+    parse_url($uri2 , PHP_URL_PATH) ?? ''
+);
+
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the
+// built-in PHP web server. This provides a convenient way to test a Laravel
+// application without having installed a "real" web server software here.
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    return false;
+}
+
+require_once __DIR__.'/public/index.php';
